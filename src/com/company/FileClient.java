45929b540a8745ab678f05fd3457be9c9f9e0bd4
arondelooijer <aron@mbbma.nl>
2020-01-16 16:04:58 +0100

    [EDIT]

--- src/com/company/FileClient.java
+++ src/com/company/FileClient.java
@@ -3,7 +3,7 @@ package com.company;
 import java.net.*;
 import java.io.*;
 
-public class FileClient {
+class FileClient {
     /**
      * Possible commands:
      * LIST / AFTP/1.0 - Shows all files
@@ -11,10 +11,10 @@ public class FileClient {
      * PUT /Tekst.txt AFTP/1.0 - Place file on server
      * DELETE AFTP/1.0 - Delete file from server
      *
-     * @param socket   De connectie met de server over een specifieke poort
-     * @param br       Buffer om de inputstream heen, haalt input uit de commandline
-     * @param ps       De communicatie met de server
-     * @param fileName Bestandsnaam van het bestand wat opgehaald/geplaatst wordt
+     * socket   De connectie met de server over een specifieke poort
+     * br       Buffer om de inputstream heen, haalt input uit de commandline
+     * ps       De communicatie met de server
+     * fileName Bestandsnaam van het bestand wat opgehaald/geplaatst wordt
     **/
     private static Socket socket;
     private static BufferedReader br;
@@ -22,9 +22,10 @@ public class FileClient {
     private static String fileName;
     private static String defaultPath = "C:\\Users\\aron1\\IdeaProjects\\AFTP_FileClient\\files\\";
 
-    public FileClient() throws IOException {
-        // De connectie wordt aangemaakt, lukt dat niet dan sluit de applicatie
-        // Tegelijkertijd wordt br geinitieerd
+    /**
+     * Creates the connection
+    **/
+    FileClient() {
         try {
             socket = new Socket("192.168.43.101", 25444);
             getStatus();
@@ -34,18 +35,17 @@ public class FileClient {
         }
     }
 
-    public void startFileClient() throws IOException {
+    void startFileClient() throws IOException {
         while(true) {
             br = new BufferedReader(new InputStreamReader(System.in));
             ps = new PrintStream(socket.getOutputStream());
 
             // Een switch die de aan de hand van het eerste woord uit de commandline de input afhandelt
             String input = selectCommand();
-            String inputArray[] = input.split(" ", 3);
+            String[] inputArray = input.split(" ", 3);
 
             switch (inputArray[0]) {
                 case "LIST":
-                case "DELETE":
                     ps.println(input);
                     getStatus();
                     continue;
@@ -59,23 +59,32 @@ public class FileClient {
                     putFile(inputArray[1]);
                     getStatus();
                     continue;
+                case "DELETE":
+                    if(inputArray[1] != null) {
+                        ps.println(input);
+                        getStatus();
+                    }
+                    continue;
                 default:
                     ps.println("COMMAND NOT FOUND");
                     getStatus();
-                    continue;
             }
         }
     }
 
-    // Deze methode laat de mogelijkheden zien met een korte beschrijving en vraagt vervolgens om de input
-    public static String selectCommand() throws IOException {
+    /**
+     * This method asks the user for input from the commandline
+     **/
+    private static String selectCommand() throws IOException {
         System.out.print(">");
 
         return br.readLine();
     }
 
-    // De methode die de status opvraagt van de server
-    public static void getStatus() {
+    /**
+     * Requests the status from the server
+     **/
+    private static void getStatus() {
         try {
             InputStream in = socket.getInputStream();
             DataInputStream clientData = new DataInputStream(in);
@@ -83,11 +92,11 @@ public class FileClient {
             System.out.println(">");
             System.out.println(status);
         } catch (Exception e) {
-            System.err.println("Exception: "+e);
+            System.err.println("Exception: " + e);
         }
     }
 
-    public static void getFile() throws IOException {
+    private static void getFile() {
         String filePath = null;
         OutputStream output = null;
         DataInputStream clientData = null;
@@ -106,18 +115,16 @@ public class FileClient {
             output.close();
         } catch (IOException ex) {
             System.err.println("Client error. Connection closed.");
-            File filePathCheck = new File(filePath);
-            Boolean defaultPathCheck = filePathCheck.exists();
-            output.close();
         }
     }
 
-    public static void putFile(String putFileName) {
+    private static void putFile(String putFileName) {
         try {
             File myFile = new File(putFileName);
             byte[] mybytearray = new byte[(int) myFile.length()];
 
             if(!myFile.exists()) {
+                System.out.println(">");
                 System.out.println("<AFTP/1.0 404 Not found");
             } else {
                 FileInputStream fis = new FileInputStream(myFile);
